e4bc6a04e91289ad67eeb135e223551480949269
Srdjan Stankovic <stankovic.srdjo@gmail.com>
2020-04-15 22:31:58 +0200

    Fix setup.py formatting

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
-        "License :: OSI Approved ::  GNU GENERAL PUBLIC LICENSE (GPL)",
+        "License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE (GPL)",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
