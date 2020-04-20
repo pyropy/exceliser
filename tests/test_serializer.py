52cf5521a81865efba5e3ca184e03ff4afc090c4
Srdjan Stankovic <stankovic.srdjo@gmail.com>
2020-04-20 13:03:40 +0200

    Add more checks to serializer tests

--- tests/test_serializer.py
+++ tests/test_serializer.py
@@ -10,3 +10,17 @@ def test_serialize_workbook(serializer_workbook_path):
     assert type(serialized_workbook) == dict
     assert 'properties' in serialized_workbook
     assert 'worksheets' in serialized_workbook
+    assert len(serialized_workbook['worksheets']
+               ) == 2, "There should be two worksheets in given workbook."
+
+    first_worksheet = serialized_workbook['worksheets'][0]
+    assert type(first_worksheet) == dict
+    assert 'rows' in first_worksheet
+    assert len(first_worksheet['rows']) == 1
+
+    first_row = first_worksheet['rows'][0]
+
+    assert type(first_row) == dict
+    assert 'cells' in first_row
+    assert len(
+        first_row['cells']) == 2, "There should be only two cells in first row of first worksheet."
