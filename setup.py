2d2838b9673a947577cd536a85bd4e94d5ef4309
Srdjan S <stankovic.srdjo@gmail.com>
2020-04-21 20:36:46 +0200

    Remove redundant requirements

--- setup.py
+++ setup.py
@@ -19,8 +19,7 @@ setup(
         "Deserializer",
     ],
     install_requires=["pytest==5.4.1",
-                      "openpyxl>=2.6.3",
-                      "pytest==5.4.1"
+                      "openpyxl>=2.6.3"
                       ],
     classifiers=[
         "Development Status :: 3 - Alpha",
